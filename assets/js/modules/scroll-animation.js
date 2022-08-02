752f4f60e42242b9e020f5b8f15415a77d6ec0a2
Andressa de Faria Souza <afariasouza@outlook.com>
2022-08-02 16:11:23 -0300

    mudança na animação do scroll

--- assets/js/modules/scroll-animation.js
+++ assets/js/modules/scroll-animation.js
@@ -6,13 +6,15 @@ export default function initScrollAnimation() {
     //Sets window height for the animation to start
     const windowHeight = window.innerHeight * 0.4;
 
-    // When the user scrolls to the window height adds class .ativo to the section
+    // When the user scrolls to the window height adds class .active to the section
     function animateScroll() {
       sections.forEach((section) => {
         const sectionTop = section.getBoundingClientRect().top - windowHeight;
         const isSectionVisible = sectionTop - windowHeight < 0;
         if (isSectionVisible) {
-          section.classList.add('ativo');
+          section.classList.add('active');
+        } else if (section.classList.contains('active')) {
+          section.classList.remove('active');
         }
       });
     }
