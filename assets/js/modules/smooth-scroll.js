990888fe0d4aa31f7af622adf06b5f8771535ac0
Andressa de Faria Souza <afariasouza@outlook.com>
2022-08-24 16:36:39 -0300

    refatoracao smooth scroll

--- assets/js/modules/smooth-scroll.js
+++ assets/js/modules/smooth-scroll.js
@@ -1,21 +1,38 @@
-export default function initSmoothScroll() {
-  //Get all menu links
-  const linksScroll = document.querySelectorAll('[data-scroll="smooth"] a[href^="#"]');
+export default class SmoothScroll {
+  constructor(links, options) {
+    //Get all menu links
+    this.linksScroll = document.querySelectorAll(links);
+    if (options === undefined) {
+      this.options = {
+        behavior: 'smooth',
+        block: 'start',
+      };
+    } else {
+      this.options = options;
+    }
 
+    this.scrollToSection = this.scrollToSection.bind(this);
+  }
   //Makes scroll slightly slower
-  function scrollToSection(event) {
+  scrollToSection(event) {
     event.preventDefault();
     const href = event.currentTarget.getAttribute('href');
     const section = document.querySelector(href);
 
-    section.scrollIntoView({
-      behavior: 'smooth',
-      block: 'start',
+    section.scrollIntoView(this.options);
+  }
+
+  addLinkEvent() {
+    // When the user clicks on the link, scroll to section
+    this.linksScroll.forEach((link) => {
+      link.addEventListener('click', this.scrollToSection);
     });
   }
 
-  // When the user clicks on the link, scroll to section
-  linksScroll.forEach((link) => {
-    link.addEventListener('click', scrollToSection);
-  });
+  init() {
+    if (this.linksScroll.length) {
+      this.addLinkEvent();
+    }
+    return this;
+  }
 }
--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,9 @@ import initBackToTop from './modules/back-to-top-button.js';
 initBackToTop();
 
 // Smooth Scroll
-import initSmoothScroll from './modules/smooth-scroll.js';
-initSmoothScroll();
+import SmoothScroll from './modules/smooth-scroll.js';
+const smoothScroll = new SmoothScroll('[data-scroll="smooth"] a[href^="#"]');
+smoothScroll.init();
 
 // Scroll Animation
 import initScrollAnimation from './modules/scroll-animation.js';
