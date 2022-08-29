7e5fc91282d639a8e9a29c2c0149c646ea23ee48
Andressa de Faria Souza <afariasouza@outlook.com>
2022-08-29 10:00:17 -0300

    fim da refatoração do scroll animation

--- assets/js/modules/scroll-animation-copy.js
+++ assets/js/modules/scroll-animation-copy.js
@@ -1,28 +1,48 @@
 export default class ScrollAnimation {
   constructor(sections) {
-    //Get all sections with .js-scroll class
+    //Gets all sections with .js-scroll class
     this.sections = document.querySelectorAll(sections);
     //Sets window height for the animation to start
-    this.windowHeight = window.innerHeight * 0.4;
+    this.windowHeight = window.innerHeight * 0.7;
 
-    this.animateScroll = this.animateScroll.bind(this);
+    this.checkDistance = this.checkDistance.bind(this);
   }
+
+  //Gets the top distance of each section
+  getDistance() {
+    this.distance = [...this.sections].map((section) => {
+      const offset = section.offsetTop;
+      return {
+        element: section,
+        offset: Math.floor(offset - this.windowHeight),
+      };
+    });
+  }
+
+  //Checks the distance of each section from the top
   // When the user scrolls to the window height adds class .active to the section
-  animateScroll() {
-    this.sections.forEach((section) => {
-      const sectionTop = section.getBoundingClientRect().top - this.windowHeight;
-      const isSectionVisible = sectionTop - this.windowHeight < 0;
-      if (isSectionVisible) {
-        section.classList.add('active');
-      } else if (section.classList.contains('active')) {
-        section.classList.remove('active');
+  checkDistance() {
+    this.distance.forEach((item) => {
+      if (window.pageYOffset > item.offset) {
+        item.element.classList.add('active');
+      } else if (item.element.classList.contains('active')) {
+        item.element.classList.remove('active');
       }
     });
   }
 
   //Activates the scroll animation
   init() {
-    this.animateScroll();
-    window.addEventListener('scroll', this.animateScroll);
+    if (this.sections.length) {
+      this.getDistance();
+      this.checkDistance();
+      window.addEventListener('scroll', this.checkDistance);
+    }
+    return this;
+  }
+
+  //Removes the animation event
+  stop() {
+    window.removeEventListener('scroll', this.checkDistance);
   }
 }
